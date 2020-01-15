5693e4fe082630ad0451a2929706f3087d231e4e
peizhen.zheng <pzzheng@thoughtworks.com>
2020-01-15 15:43:54 +0800

    should calculate waiting time ignoring the starting kilometers.

--- TaxiPriceCalculator.Test/WaitingTimeCalculatorSpec.cs
+++ TaxiPriceCalculator.Test/WaitingTimeCalculatorSpec.cs
@@ -8,11 +8,24 @@ namespace TaxiPriceCalculator.Test
         [Theory]
         [InlineData(2, 0, new[]{100/3600.0, 119/3600.0, 120/3600.0, 121/3600.0})]
         [InlineData(1, 1, new[]{100/3600.0, 119/3600.0, 120/3600.0, 121/3600.0})]
+        [InlineData(0, 4, new[]{100/3600.0, 119/3600.0, 120/3600.0, 121/3600.0})]
+        [InlineData(0, 5, new[]{100/3600.0, 119/3600.0, 120/3600.0, 121/3600.0})]
         void should_calculate_waiting_time_when_speed_is_lower_than_120_km_per_hour(int waitingSeconds, int startIndex, double[] kilosBySecond)
         {
             var waitingTimeCalculator = new WaitingTimeCalculator(kilosBySecond);
 
             Assert.Equal(waitingSeconds, waitingTimeCalculator.CalcWaitingTime(startIndex, kilosBySecond.Length));
+        }
+        
+        [Theory]
+        [InlineData(2, 0, new[]{100/3600.0, 119/3600.0, 120/3600.0, 121/3600.0})]
+        [InlineData(0, 0.1, new[]{121/3600.0, 120/3600.0, 119/3600.0, 121/3600.0})]
+        [InlineData(1, 0.1, new[]{121/3600.0, 120/3600.0, 119/3600.0, 121/3600.0, 119/3600.0})]
+        void should_calculate_waiting_time_ignoring_starting_mileage_and_considering_speed(int waitingSeconds, double startMileages, double[] kilosBySecond)
+        {
+            var waitingTimeCalculator = new WaitingTimeCalculator(kilosBySecond);
+
+            Assert.Equal(waitingSeconds, waitingTimeCalculator.CalcWaitingTime(startMileages));
         }
     }
 }
--- TaxiPriceCalculator/WaitingTimeCalculator.cs
+++ TaxiPriceCalculator/WaitingTimeCalculator.cs
@@ -22,9 +22,33 @@ namespace TaxiPriceCalculator
         /// <exception cref="NotImplementedException"></exception>
         public long CalcWaitingTime(int startIndex, int endIndex)
         {
+            if (startIndex >= endIndex) return 0;
             return _kilosBySecond
                 .GetRange(startIndex, endIndex - startIndex)
                 .Count(k => k * 3600 < WAITING_SPEED_THRESHOLD);
+        }
+        
+        public long CalcWaitingTime(double startMileages)
+        {
+            return CalcWaitingTime(GetThreshIndex(startMileages), _kilosBySecond.Count);
+        }
+
+        private int GetThreshIndex(double startMileages)
+        {
+            var distance = 0.0;
+            var threshIndex = 0;
+            for (; threshIndex < _kilosBySecond.Count; threshIndex++)
+            {
+                distance = distance + _kilosBySecond[threshIndex];
+                if (distance >= startMileages) break;
+            }
+
+            if (startMileages > 0)
+            {
+                threshIndex++;
+            }
+
+            return threshIndex;
         }
     }
 }
